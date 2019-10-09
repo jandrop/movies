4ccd651e79bd202e33bafdb16e7287ae1e940d2f
Jandro <alejandro.platas@mobgen.com>
2019-10-09 12:34:48 +0200

    Add datapolicy in the interactor call

--- lib/domain/interactor/get_top_rated_movies_interactor.dart
+++ lib/domain/interactor/get_top_rated_movies_interactor.dart
@@ -1,13 +1,15 @@
 import 'package:the_movie_db/data/repository/movies_repository.dart';
 import 'package:the_movie_db/domain/model/Movie.dart';
 
+import '../data_policy.dart';
+
 class GetTopRatedMoviesInteractor {
   MoviesRepository _repository;
 
   GetTopRatedMoviesInteractor(this._repository);
 
   Future<List<Movie>> run() async {
-    var response = await _repository.getTopRatedMovies();
+    var response = await _repository.getTopRatedMovies(DataPolicy.REMOTE);
     if (response.isNotEmpty) {
       return response
           .map((movie) => Movie(
