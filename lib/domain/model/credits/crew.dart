0e93148c3db155582fb11e058c9f6eb49c94b6c4
Jandro <alejandro.platas@mobgen.com>
2019-10-11 19:21:29 +0200

    Create interactor and entities

---
+++ lib/domain/model/credits/crew.dart
@@ -0,0 +1,17 @@
+class Crew {
+  String creditId;
+  String department;
+  int gender;
+  int id;
+  String job;
+  String name;
+  String profilePath;
+
+  Crew({
+    this.creditId,
+    this.department,
+    this.job,
+    this.name,
+    this.profilePath,
+  });
+}
