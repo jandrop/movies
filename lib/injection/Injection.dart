8616e3f45dd0ef28f763798d6dee3d22eba7172e
Jandro <alejandro.platas@mobgen.com>
2019-10-11 19:21:51 +0200

    Add to injection class

--- lib/injection/Injection.dart
+++ lib/injection/Injection.dart
@@ -1,7 +1,10 @@
 import 'package:the_movie_db/data/datasources/local/database_helper.dart';
 import 'package:the_movie_db/data/datasources/local/movies_local_datasource.dart';
+import 'package:the_movie_db/data/datasources/remote/credits_remote_datasource.dart';
 import 'package:the_movie_db/data/datasources/remote/movies_remote_datasource.dart';
+import 'package:the_movie_db/data/repository/credits_repository.dart';
 import 'package:the_movie_db/data/repository/movies_repository.dart';
+import 'package:the_movie_db/domain/interactor/get_credits_interactor.dart';
 import 'package:the_movie_db/domain/interactor/get_next_movies_interactor.dart';
 import 'package:the_movie_db/domain/interactor/get_popular_movies_interactor.dart';
 import 'package:the_movie_db/domain/interactor/get_top_rated_movies_interactor.dart';
@@ -38,5 +41,17 @@ class Injection {
     return GetTopRatedMoviesInteractor(
       _providesMoviesRepository(),
     );
+  }
+
+  static _providesCreditsRemoteDataSource() {
+    return CreditsRemoteDataSourceImp();
+  }
+
+  static _providesCreditsRepository() {
+    return CreditsRepositoryImp(_providesCreditsRemoteDataSource());
+  }
+
+  static providesGetCreditsInteractor() {
+    return GetCreditsInteractor(_providesCreditsRepository());
   }
 }
